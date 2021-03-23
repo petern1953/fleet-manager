0aec6ed5d3f1764157f22347a2e83e7ffd507df8
petern1953 <petern1953@gmail.com>
2021-03-23 18:22:21 +0100

    up to nglf - látod, hogy nem látod

--- src/app/part/data-cell/data-cell.component.ts
+++ src/app/part/data-cell/data-cell.component.ts
@@ -3,14 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-cell',
   templateUrl: './data-cell.component.html',
-  styleUrls: ['./data-cell.component.scss']
+  styleUrls: ['./data-cell.component.scss'],
 })
 export class DataCellComponent implements OnInit {
   @Input() cellData: string = '';
+  @Input() colType: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -6,28 +6,28 @@ import { Injectable } from '@angular/core';
 export class ConfigService {
   cols = {
     drivers: [
-      { key: 'id', text: '#' },
-      { key: 'name', text: 'Name' },
-      { key: 'email', text: 'Email' },
-      { key: 'phone', text: 'Phone' },
-      { key: 'city', text: 'City' },
-      { key: 'address', text: 'Address' },
+      { key: 'id', text: '#', type: 'plain' },
+      { key: 'name', text: 'Name', type: 'text' },
+      { key: 'email', text: 'Email', type: 'email' },
+      { key: 'phone', text: 'Phone', type: 'number' },
+      { key: 'city', text: 'City', type: 'text' },
+      { key: 'address', text: 'Address', type: 'text' },
     ],
     vehicles: [
-      { key: 'id', text: '#' },
-      { key: 'lp', text: 'LP.' },
-      { key: 'year', text: 'Year' },
-      { key: 'manufacturer', text: 'Man.' },
-      { key: 'consumption', text: 'Cons.' },
-      { key: 'engine', text: 'Eng.' },
+      { key: 'id', text: '#', type: 'plain' },
+      { key: 'lp', text: 'LP.', type: 'text' },
+      { key: 'year', text: 'Year', type: 'number' },
+      { key: 'manufacturer', text: 'Man.', type: 'text' },
+      { key: 'consumption', text: 'Cons.', type: 'text' },
+      { key: 'engine', text: 'Eng.', type: 'text' },
     ],
     fuelings: [
-      { key: 'id', text: '#' },
-      { key: 'vehicle', text: 'Vehicle' },
-      { key: 'driver', text: 'Driver' },
-      { key: 'amount', text: 'Amount' },
-      { key: 'date', text: 'Date' },
-    ]
+      { key: 'id', text: '#', type: 'plain' },
+      { key: 'vehicle', text: 'Vehicle', type: 'text' },
+      { key: 'driver', text: 'Driver', type: 'text' },
+      { key: 'amount', text: 'Amount', type: 'number' },
+      { key: 'date', text: 'Date', type: 'text' },
+    ],
   };
 
   constructor() {}
